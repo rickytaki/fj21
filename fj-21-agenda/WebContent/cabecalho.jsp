c96109500d21a4110e3e86d6f0bf292ee39ea734
rickytaki <rickytaki@yahoo.com.br>
2014-07-23 17:03:43 -0300

    aula 3

---
+++ fj-21-agenda/WebContent/cabecalho.jsp
@@ -0,0 +1,3 @@
+	<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+	<img src="<c:url value="/imagens/caelum.png" />"/>
+	<h2> Agenda de Contatos do(a) Ricardo</h2>
--- fj-21-agenda/src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
+++ fj-21-agenda/src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -49,7 +50,9 @@ public class AdicionaContatoServlet extends HttpServlet {
 			ContatoDao dao = new ContatoDao();
 			dao.adiciona(contato);
 			
-			out.println("<html><body>Contato " + contato.getNome() + " adicionado com sucesso</body></html>");
+			RequestDispatcher rd= request.getRequestDispatcher("/contato-adicionado.jsp");
+			rd.forward(request,response);
+			
 		}
 	  
 
